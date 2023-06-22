51bf3dd31c36f1a3d10e1798da3dcda5b6e14dd2
hhoang0811 <dthhoang0811@gmail.com>
2023-06-23 02:21:30 +0700

    call Api - axios

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header";
@@ -5,10 +6,32 @@ import NotFound from './components/NotFound'
 import AddEditPage from './features/photos/pages/AddEdit';
 import MainPage from "./features/photos/pages/Main";
 import './App.scss'
+import productApi from './api/productApi';
 
 // const Photo = React.lazy(() => import ('./features/photos'));
 
 function App() {
+
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    const fetchProductList = async () => {
+      try {
+        const params = {
+          _page: 1,
+          _limit: 10,
+
+        };
+        const response = await productApi.getAll(params);
+        console.log(response);
+        setProductList(response.data);
+      } catch (error) {
+        console.log('Failed to fetch product list: ', error);
+      }
+    }
+
+    fetchProductList();
+  }, []);
 
   return (
     <div className="photo-app">
